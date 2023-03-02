6abc1a2048a4936d82c4b9bc9d330deb50b88917
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-03-02 23:34:44 +0100

    converted to jsx because fuck react

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e36201878c70c79"; }
+/******/ 		__webpack_require__.h = function() { return "3a7a5dbe40b0f50a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -33,26 +33,61 @@ export default function Home() {
 
   return (
     <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
-
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+      <header>
+        <h1>Create-a-friend</h1>
+        <nav>
+            <a>a</a>
+            <a>b</a>
+            <a>c</a>
+        </nav>
+    </header>
+    <article>
+        
+    </article>
+    <article>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/320px-A_black_image.jpg" alt="ai generated art" id="generatedImg"/>
         <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
+           <input
+             type="text"
+             name="animal"
+             placeholder="Enter an animal"
+             value={animalInput}
+             onChange={(e) => setAnimalInput(e.target.value)}
+           />
+           <input type="submit" value="Generate names" />
+           <div className={styles.result}>{result}</div>
+         </form> 
+    </article>
+    <footer>
+        Images and chat provided by OpenAI Api 
+    </footer>
     </div>
+
+
+
+
+    
+    // <div>
+    //   <Head>
+    //     <title>OpenAI Quickstart</title>
+    //     <link rel="icon" href="/dog.png" />
+    //   </Head>
+
+    //   <main className={styles.main}>
+    //     <img src="/dog.png" className={styles.icon} />
+    //     <h3>Name my pet</h3>
+    //     <form onSubmit={onSubmit}>
+    //       <input
+    //         type="text"
+    //         name="animal"
+    //         placeholder="Enter an animal"
+    //         value={animalInput}
+    //         onChange={(e) => setAnimalInput(e.target.value)}
+    //       />
+    //       <input type="submit" value="Generate names" />
+    //     </form>
+    //     <div className={styles.result}>{result}</div>
+    //   </main>
+    // </div>}
   );
 }
