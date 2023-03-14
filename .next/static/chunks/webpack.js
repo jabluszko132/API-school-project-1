de948ea06250127814f126cb6863c9a68ce36e9d
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-03-14 19:35:04 +0100

    fixed the chat functionality achieved

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50ade615605a5cfd"; }
+/******/ 		__webpack_require__.h = function() { return "aeb9d0654b36a1e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,10 @@ export default function Home() {
 
 
   async function onSubmit(event) {
-    setResult(chat.innerText+"User: "+messageInput+"\n");
+    setResult(chat.innerText+"User: "+messageInput);
+    chat.innerHTML+="User: "+messageInput;
     // console.log(event);
-    setMessageInput(result +"\n"+ thePrompt.value);
+    setMessageInput(result + thePrompt.value);
     event.preventDefault();
     try {
       const response = await fetch("/api/generate", {
@@ -25,8 +26,8 @@ export default function Home() {
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
-      setResult(chat.innerText+"\n Bot: " + data.result);
+      setResult(chat.innerText+"Bot: " + data.result);
+      chat.innerHTML += "<br>Bot: "+data.result;
       setMessageInput("");
     } catch(error) {
       console.error(error);
@@ -35,21 +36,31 @@ export default function Home() {
   }
   return (
     <div className={styles.main}>
+      <p id="up"></p>
       <header>
         <h1>Create-a-friend</h1>
         <nav>
-            <a>a</a>
-            <a>b</a>
-            <a>c</a>
+            <a href="#up">Start</a>
+            <a href="#theForm">Chat</a>
+            <a href="#about">About</a>
         </nav>
     </header>
-    <article>
-        
+    <article id="about">
+        <h2>About our project</h2>
+        <p>
+          So for our API project we have decided to make a site with an in-built chatbot. <br/>
+          The chatbot model is text-babbage-001 and is provided by OpenAI - the company that created the famous ChatGPT. <br/>
+          We have also decided we wanted to learn something new and challenge ourselves a bit, so the entire website is generated using ReactJS. <br/>
+          My job was mainly to create the structre of the website and to plug the babbage into it. Although the job wasn't easy (API key rotation was so annoying)  
+          I did manage to do it. I got the most useful informations from youtube tutorials and OpenAI Api documentation as well as their opensource test website.  <br/>
+          The CSS (done mostly by Kamil) is a module - every selector inside it had to begin with class or id. <br/>
+          Because our project is made in React and our CSS is a module our code can be used not only as a website of its own but also as an element of a bigger one.
+        </p>
     </article>
     <article>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/320px-A_black_image.jpg" alt="ai generated art" id="generatedImg"/>
+        <img src="https://cdn.pixabay.com/photo/2023/03/06/21/16/artificial-intelligence-7834467_960_720.jpg" alt="ai"/>
         
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} id="theForm">
            <input
              id="thePrompt"
              type="text"
@@ -58,9 +69,13 @@ export default function Home() {
              value={messageInput}
              onChange={(e) => setMessageInput(e.target.value)}
            />
-           <input type="submit" value="Generate names"/>
+           <input type="submit" value="Send"/>
            <br/>
-           <textarea id="chat" className={styles.result} readOnly value={result}/>
+           <div id="chat">
+            User: testowa konwersacja weź to później usuń
+            <br/>
+            Bot: Ok, ale słuchaj: Ea ad cillum adipisicing aliquip ullamco veniam. Cillum et enim occaecat adipisicing et ad mollit pariatur. Dolore nulla aliqua Lorem ex et commodo. Sint pariatur velit irure Lorem velit mollit esse incididunt laboris fugiat aliquip enim officia. Aliqua in nisi dolore exercitation amet Lorem enim ullamco. Proident voluptate proident excepteur consequat aliquip.
+           </div>
            <br/>
            {/* <div className={styles.result} id="chat">{result}</div> */}
          </form> 
@@ -71,31 +86,6 @@ export default function Home() {
     </div>
 
 
-
-
-    
-    // <div>
-    //   <Head>
-    //     <title>OpenAI Quickstart</title>
-    //     <link rel="icon" href="/dog.png" />
-    //   </Head>
-
-    //   <main className={styles.main}>
-    //     <img src="/dog.png" className={styles.icon} />
-    //     <h3>Name my pet</h3>
-    //     <form onSubmit={onSubmit}>
-    //       <input
-    //         type="text"
-    //         name="animal"
-    //         placeholder="Enter an animal"
-    //         value={animalInput}
-    //         onChange={(e) => setAnimalInput(e.target.value)}
-    //       />
-    //       <input type="submit" value="Generate names" />
-    //     </form>
-    //     <div className={styles.result}>{result}</div>
-    //   </main>
-    // </div>}
   );
 }
 
